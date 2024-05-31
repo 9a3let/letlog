7cc98bb3fe859ff01a75d0ffcfb764b06e1cbe7a
9a3let <horvatic.leonardo1@gmail.com>
2024-05-31 12:22:19 +0200

    commit 2

---
+++ src/main/java/com/leo/LogConfig.java
@@ -0,0 +1,9 @@
+package com.leo;
+
+public class LogConfig {
+    public static class MainFrame {
+        public static int sizeX = 800;
+        public static int sizeY = 450;
+    }
+
+}
--- src/main/java/com/leo/Main.java
+++ src/main/java/com/leo/Main.java
@@ -3,10 +3,12 @@ package com.leo;
 import javax.swing.JFrame;
 
 public class Main {
+
     public static void main(String[] args) {
+
         JFrame mainFrame = new JFrame();
 
-        mainFrame.setSize(800, 450);
+        mainFrame.setSize(LogConfig.MainFrame.sizeX, LogConfig.MainFrame.sizeY);
 
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainFrame.setLayout(null);
