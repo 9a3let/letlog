bd374a226c863aeb910443772b154573fdfce997
9a3let <horvatic.leonardo1@gmail.com>
2024-05-31 11:49:27 +0200

    first commit

---
+++ src/main/java/com/leo/Main.java
@@ -0,0 +1,15 @@
+package com.leo;
+
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame mainFrame = new JFrame();
+
+        mainFrame.setSize(800, 450);
+
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setLayout(null);
+        mainFrame.setVisible(true);
+    }
+}
