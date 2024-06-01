b6c7a7fca61b909958177da8397f52490668e7aa
9a3let <horvatic.leonardo1@gmail.com>
2024-06-01 15:47:12 +0200

    removed unused import

--- src/main/java/com/leo/Main.java
+++ src/main/java/com/leo/Main.java
@@ -3,7 +3,6 @@ package com.leo;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
